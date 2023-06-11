d2130e051ab341020482658ca30f736d2f76c6a2
Prince Kumar <princesansaraj@gmail.com>
2023-06-11 13:11:07 +0530

    Hassing password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// jwt secret key which is signature of jwttoken
+const JWT_SECRET = "helloprincekumar";
 
 
 // create a user using : POST "/api/auth/createuser". Doesn't required login
@@ -25,13 +30,27 @@ router.post('/createuser',[
       if (user) {
         return res.status(400).json({error: "This email already exist"})
       }
+
+      // hassing the password
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       // create User
       user = await User.create({
           name: req.body.name,
           email: req.body.email,
-          password: req.body.password,
+          password: secPass,
         })
-        res.json(user);
+
+        // jwt authtoken
+        const data = {
+          user: {
+            id: user.id
+          }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+       
+        // res.json(user);
+        res.json({authtoken});
         
     } catch (error) {
       console.log(error.message);
