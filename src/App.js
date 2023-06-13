f484b4b18c74086a869892e08b01d47954e0e60e
Prince Kumar <princesansaraj@gmail.com>
2023-06-13 12:46:55 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Navbar from './components/Navbar'; 
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>CLOUDNOTE</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/About" element={<About/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const About = () => {
+  return (
+    <div>
+      This ia About.
+    </div>
+  )
+}
+
+export default About
