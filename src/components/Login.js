e06d091ade32a29fb6f685f56a68e61b829105af
Prince Kumar <princesansaraj@gmail.com>
2023-09-26 17:16:28 +0530

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login = (props) => {
 
     const handleSubmit = async (e)=>{
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/api/auth/login',{
+        const response = await fetch('https://react-cloudnote.onrender.com/api/auth/login',{
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
